e50c4bb02f4cbb2197b3a6ab1ce36a52c153f800
rahatulahsan <rahatulahsan@gmail.com>
2023-08-06 13:28:50 +0600

    Featured Box Design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{html,js}',
+  './components/**/*.{html,js}',,
+  "./src/**/*.{html,js}",
+  "'*.{html,js}'"],
+  theme: {
+    fontFamily:{
+      sans: ['Open Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
